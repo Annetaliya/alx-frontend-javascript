c214b855e00cc34258d0e8cf2e1265d11a9051c3
Annetaliya <annetaliya@gmail.com>
2023-06-29 18:37:07 +0300

    implementing getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
+    }
   }
 
   get name() {
@@ -18,26 +30,26 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string') {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
       throw new TypeError('Name must be a string');
     }
-
-    this._name = name;
   }
 
   set length(length) {
-    if (typeof length !== 'number') {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
       throw new TypeError('Length must be a number');
     }
-
-    this._length = length;
   }
 
   set students(students) {
     if (Array.isArray(students)) {
+      this._students = students;
+    } else {
       throw new TypeError('Students must be an Array');
     }
-
-    this._name = students;
   }
 }
