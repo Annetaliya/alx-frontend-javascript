f1976229332ef7f40285ce9a1a836f1c86453648
Annetaliya <annetaliya@gmail.com>
2023-07-04 15:47:31 +0300

    updating map by value

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(list) {
   try {
-    const newMap = Array.from(groceries.keys());
+    const newMap = Array.from(list.keys());
 
     return (newMap.filter((item) => list.get(item) === 1).map((item) => list.set(item, 100)));
   } catch (err) {
