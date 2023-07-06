71513598b14cd3dfe95a68c107d96c5118d8e745
Annetaliya <annetaliya@gmail.com>
2023-07-06 13:23:48 +0300

    implementing type literals

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,3 +59,16 @@ function executeWork(employee: Director | Teacher) {
         console.log(employee.workTeacherTasks());
     }
 }
+
+
+//type literals
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
